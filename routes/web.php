3c9c60043154a77f0a0ab6a827fdac1d5c3eb67b
sabuj37 <89691756+sabuj37@users.noreply.github.com>
2024-10-30 17:10:25 +0600

    AdminTemplateUpload

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\frontController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home',[
+    frontController::class,
+    'home'
+])->name('adminHome');
